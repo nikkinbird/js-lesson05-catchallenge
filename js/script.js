8775473c8aa606cc41c6a7aa5684c120e5242b21
nikkinbird <nikki.bollman@gmail.com>
2021-05-22 17:07:54 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var button = document.querySelector(".show-cat");
+//console.log(button);
+
+var cat = document.querySelector(".cat");
+//console.log(cat);
+
+button.addEventListener("click", function () {
+  if (cat.classList.contains("show")) {
+    cat.classList.remove("show");
+    button.innerText = "Wait! Come back!";
+    button.classList.add("disappear");
+  } else {
+    cat.classList.add("show");
+    button.innerText = "Shoo cat!";
+    button.classList.remove("disappear");
+  }
+});
